e66848e9283efde49909f08a6e5fd36f82dfccfd
CHRIS_LEE <chrismlee87@gmail.com>
2021-09-22 23:38:46 -0700

    Update songs albums

--- songs_albums.sql
+++ songs_albums.sql
@@ -7,6 +7,7 @@
  * Note that album - song is a one-to-many relationship, so no bridge table is needed.
  */
 
+
 CREATE TABLE Albums (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(70) NOT NULL,
@@ -14,9 +15,12 @@ CREATE TABLE Albums (
     year_published INTEGER NOT NULL
 );
 
-/* 
- * TODO: Insert at least 4 rows of data into the songs table. You can change up the albums as well. :)
- */
+CREATE TABLE Songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(70) NOT NULL,
+    album_id INTEGER,
+    FOREIGN KEY(album_id) REFERENCES Albums(id)
+);
 
 INSERT INTO Albums
     (name, artist, year_published)
@@ -28,30 +32,25 @@ VALUES
     ('California', 'Blink-182', 2016)
 ;
 
-/* Required to get result in column format */
-.headers on
-.mode column
-
-
-/* Queries */
-
--- SELECT * FROM Songs;
-
-SELECT * FROM Albums;
 
 /* 
- * TODO: Write a table join query to construct a table of Song Name : Album Name
+ * TODO: Insert at least 4 rows of data into the songs table. You can change up the albums as well. :)
  */
 
-/*
- * TODO: Find all albums published between 1970 and 1980.
- */
+INSERT INTO Songs  
+    (name)
+VALUES
+    ('On the Run'),
+    ('Come Together'),
+    ('Hotel California'),
+    ('Cover Me'),
+    ('Bored to Death')
+;
 
 /*
- * TODO: Find all songs on albums published between 1970 and 1980. 
- *(Hint: Use a table join.)
- */
+Queries
 
-/*
- * TODO: Find all songs on albums with names containing 'California'.
- */
+*/
+
+SELECT * FROM Albums 
+SELECT * FROM Songs
